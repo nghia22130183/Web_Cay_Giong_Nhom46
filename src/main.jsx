3bb48d4798aaf60ba8439c238b3a954de419397e
nghia22130183 <22130183@st.hcmuaf.edu.vn>
2026-01-16 21:41:30 +0700

    adding hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import './index.css'
 
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
 )
